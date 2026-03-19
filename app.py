190efea425e03b037af567405df0cde942394d05
Amit Sharma <amitsharmar249@gmail.com>
2026-03-19 15:54:10 +0530

    fix nltk punkt error

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
-nltk.download('omw-1.4')
+nltk.download('punkt_tab')
 import streamlit as st
 import pickle
 import string
